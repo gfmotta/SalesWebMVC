eaab80c4433cb78f9b79bf49c6734c72bf0a66d8
Gustavo Motta <guforatini@gmail.com>
2021-12-27 19:13:42 -0300

    Creating the other entities and second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; }
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord saleRecord)
+        {
+            Sales.Add(saleRecord);
+        }
+
+        public void RemoveSales(SalesRecord saleRecord)
+        {
+            Sales.Remove(saleRecord);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(x => x.Date >= initial && x.Date <= final).Sum(x => x.Amount);
+        }
+    }
+}
