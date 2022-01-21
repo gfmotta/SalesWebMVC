b17b175aca7f1ff1c59f73799343f71179ca9b5b
Gustavo Motta <guforatini@gmail.com>
2022-01-21 11:34:15 -0300

    Sales search, simple and grouping search

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch(DateTime? fromDate, DateTime? untilDate)
+        {
+            if (!fromDate.HasValue)
+            {
+                fromDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+
+            if (!untilDate.HasValue)
+            {
+                untilDate = DateTime.Now;
+            }
+
+            ViewData["fromDate"] = fromDate.Value.ToString("yyyy-MM-dd");
+            ViewData["untilDate"] = untilDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _salesRecordService.FindByDateAsync(fromDate, untilDate);
+
+            return View(result);
+        }
+
+        public async Task<IActionResult> GroupingSearch(DateTime? fromDate, DateTime? untilDate)
+        {
+            if (!fromDate.HasValue)
+            {
+                fromDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+
+            if (!untilDate.HasValue)
+            {
+                untilDate = DateTime.Now;
+            }
+
+            ViewData["fromDate"] = fromDate.Value.ToString("yyyy-MM-dd");
+            ViewData["untilDate"] = untilDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _salesRecordService.FindByDateGroupingAsync(fromDate, untilDate);
+
+            return View(result);
+        }
+    }
+}
---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SalesRecordService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? fromDate, DateTime? untilDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (fromDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= fromDate.Value);
+            }
+
+            if (untilDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= untilDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? fromDate, DateTime? untilDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (fromDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= fromDate.Value);
+            }
+
+            if (untilDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= untilDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
+    }
+}
